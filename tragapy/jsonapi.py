2a4775ab64c2b441370f270f9652eb75451a0b90
Max <dr.maximilian.scherer@gmail.com>
2021-02-02 22:27:42 +0100

    added workspace and function stubs

---
+++ tragapy/jsonapi.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+def traga_quote(isin:str)->dict:
+    return {}
+
+def traga_top()->dict:
+    return {}
+
+def traga_ticks(isin:str, from_id:int)->dict:
+    return {}
